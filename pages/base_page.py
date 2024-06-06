e5c19ae8b61b5ac60ce90f4ec8b7cd2eed6e9581
Igor Chernivoy <igor.chernivoy@gmail.com>
2024-06-06 16:48:28 +0300

    Added new functions for elements

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,5 @@
+import allure
+from selenium.common import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -14,8 +16,30 @@ class BasePage:
         element.clear()
         element.send_keys(text)
 
-    def find_elements(self, *locator):
-        return WebDriverWait(self.browser, 10).until(EC.presence_of_all_elements_located(locator))
+    def element_is_visible(self, locator, timeout=10):
+        with allure.step('Check visibility element on the form'):
+            try:
+                return WebDriverWait(self.browser, timeout).until(EC.visibility_of_element_located(locator))
+            except TimeoutException:
+                return False
+
+    def elements_are_visible(self, locator, timeout=10):
+        return WebDriverWait(self.browser, timeout).until(EC.visibility_of_all_elements_located(locator))
+
+    def element_is_present(self, locator):
+        return WebDriverWait(self.browser, 10).until(EC.presence_of_element_located(locator))
+
+    # def elements_are_present(self, locator):
+    #     return WebDriverWait(self.browser, 10).until(EC.presence_of_all_elements_located(locator))
+    #
+    # def element_is_not_visible(self, locator):
+    #     return WebDriverWait(self.browser, 10).until(EC.invisibility_of_element(locator))
+    #
+    # def element_is_clickable(self, locator):
+    #     return WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable( locator))
+    #
+    # def go_to_element(self, element):
+    #     self.browser.execute_script("argument[0].scrollInfoView();", element)
 
     def click(self, *locator):
         element = self.find(*locator)
