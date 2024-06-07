77b257ee0a9a6f0c5bb74d990ce039e93e0f0c23
Igor Chernivoy <igor.chernivoy@gmail.com>
2024-06-07 16:04:46 +0300

    added Dashboard page

---
+++ pages/dashboard_page.py
@@ -0,0 +1,6 @@
+from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+import allure
