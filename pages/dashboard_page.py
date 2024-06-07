2e8b9ad71cf8a24b39ffb9a14b97aa332b3bdbc8
Igor Chernivoy <igor.chernivoy@gmail.com>
2024-06-07 16:27:43 +0300

    added locators on Dashboard page

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -4,3 +4,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import allure
+
+btn_chevronleft_locator = (By.XPATH, "//dx-button[@icon='chevronleft' and @role='button']")
+btn_preferences_locator = (By.XPATH, "//dx-button[@icon='preferences' and @role='button']")
+
+btn_abmelden_locator = (By.XPATH, "//div[@role='button' and @aria-label='Abmelden' and contains(@class, 'dx-button-danger')]")
+btn_conf_locator = (By.XPATH, "//div[@role='button' and @aria-label ='Konfiguration' and contains(@class, 'dx-widget dx-button dx-button-mode-outlined dx-button-normal dx-button-has-text')]")
+btn_cansel_locator = (By.XPATH, "//div[@role='button' and @aria-label ='Abbrechen' and contains(@class, 'dx-widget dx-button dx-button-mode-outlined dx-button-normal dx-button-has-text')]")
