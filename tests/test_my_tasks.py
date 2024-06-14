8496e25817f88ee81d10c98543fb15adbc6f634d
Igor Chernivoy <igor.chernivoy@gmail.com>
2024-06-14 11:52:56 +0300

    refactoring

--- tests/test_my_tasks.py
+++ tests/test_my_tasks.py
@@ -5,18 +5,21 @@ from selenium import webdriver
 from time import sleep
 import allure
 
+task_name = 'WEB TASK 0001'
+
 
 @allure.feature('MyTasksPage')
-@allure.story('Check count tasks on Dashboard page')
+@allure.story('Check find task on myTask page')
 def test_my_tasks(login):
     dashboard_page = DashboardPage(login)
     dashboard_page.label_dashboard_click()
-    sleep(5)
+    sleep(2)
     dashboard_page.area_my_tasks_click()
-    sleep(10)
+    # my task page
     my_tasks_page = MyTasksPage(login)
-    my_tasks_page.search_field_enter_value('Cyclic task 1')
-    sleep(5)
-    my_tasks_page.table_contains_text('Cyclic task 1')
-    sleep(5)
+    my_tasks_page.label_data_grid_locator_is_visible()
+    my_tasks_page.search_field_enter_text(task_name)
+    my_tasks_page.label_data_grid_locator_is_visible()
+    my_tasks_page.table_contains_text(task_name)
+    sleep(1)
 
