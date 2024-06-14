07b547f79f11d99667c27143ef7be77d56a01050
Igor Chernivoy <igor.chernivoy@gmail.com>
2024-06-14 11:53:16 +0300

    refactoring

--- pages/my_tasks_page.py
+++ pages/my_tasks_page.py
@@ -16,7 +16,7 @@ class MyTasksPage(BasePage):
     def __init__(self, browser):
         super().__init__(browser)
 
-    def table_contains_text(self, value):
+    def table_contains_text_old(self, value):
         with allure.step(f'Find "{value}" value in the table by text in cell'):
             is_bool = self.label_data_grid_empty_locator_is_visible()
             if not is_bool:
@@ -51,13 +51,55 @@ class MyTasksPage(BasePage):
                 with allure.step(f'table was not present or empty'):
                     print("table was not present or empty")
 
+    def table_contains_text(self, value):
+        with allure.step(f'Find "{value}" value in the table by text in cell'):
+            if self.label_data_grid_empty_locator_is_visible():
+                with allure.step('Table was not present or empty'):
+                    print("Table was not present or empty")
+                return
+
+            table = self.find(data_grid_locator)
+            is_find_element = False
+
+            # Обход строк таблицы
+            for row in table.find_elements(By.TAG_NAME, 'tr'):
+                # Получение ячеек из строки
+                cells = row.find_elements(By.TAG_NAME, 'td')
+
+                if not cells:
+                    # Печать данных заголовка в консоль
+                    print("This is Header Row")
+                    print(row.text.replace(" ", "\t\t"))
+                    continue
+
+                # Обход ячеек в строке
+                for cell in cells:
+                    if cell.text == value:
+                        try:
+                            print(f"Value in row : '{cell.text}' was clicked.")
+                            cell.click()
+                            is_find_element = True
+                        except Exception as e:
+                            print(f"Exception occurred: {e}")
+                        break
+
+                if is_find_element:
+                    break
+
+            if not is_find_element:
+                with allure.step(f"Value '{value}' was not found in the table after checking all cells"):
+                    print(f"Value '{value}' was not found in the table after checking all cells")
+
     def search_field_click(self):
         with allure.step(f'Click in the search field'):
             return self.click(search_field_locator)
 
-    def search_field_enter_value(self, value):
+    def search_field_enter_text(self, value):
         with allure.step(f'Enter "{value}" value in search field '):
             self.enter_text(value, search_field_locator)
+
+    def label_data_grid_locator_is_visible(self):
+        return self.element_is_visible(data_grid_locator)
 
     def label_data_grid_empty_locator_is_visible(self):
         return self.element_is_visible(data_grid_empty_locator)
